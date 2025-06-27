5c7da64f9e2833bb11dd59d1dad7408715962b44
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-27 09:16:29 +0200

    refactor(utils): update `file_name` to use `Path`
    Replace `PathBuf` with `Path` in `file_name` helper.

--- crates/utils/src/pathbuf.rs
+++ crates/utils/src/pathbuf.rs
@@ -1,15 +1,12 @@
 //! Various helper methods for working with Rust's [`PathBuf`].
 
-use std::path::PathBuf;
+use std::path::Path;
 
 /// Retrieves the file name from the given [`PathBuf`].
 ///
 /// This method is lossy in case of invalid UTF-8 characters, see [`OsStr::to_string_lossy`]
 #[inline]
-pub fn file_name(path: &PathBuf) -> Option<String> {
-    let Some(file_name) = path.file_name() else {
-        return None;
-    };
-
-    Some(file_name.to_string_lossy().to_string())
+#[must_use]
+pub fn file_name(path: &Path) -> Option<String> {
+    Some(path.file_name()?.to_string_lossy().to_string())
 }
